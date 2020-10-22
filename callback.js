41751e291baceca3233c183722167583f6dbb228
Bernardo Silveira <bernardo@bsilveira.com>
2020-10-22 16:52:00 +0200

    callback done

---
+++ callback.js
@@ -0,0 +1,29 @@
+console.time('timer');
+const getBanana = function (callback) {
+    setTimeout(() => {
+        callback('banana');
+    }, 2000);
+};
+
+const getApple = function (callback) {
+    setTimeout(() => {
+        callback('apple');
+    }, 1000);
+};
+
+const addFruit = function (fruit1, fruit2) {
+    return fruit1 + ' ' + fruit2;
+}
+
+console.log('::Start::');
+
+getBanana(banana => {
+    console.log('We have the banana!');
+    getApple(apple => {
+        console.log('We have the apple!');
+        console.log(addFruit(banana, apple));
+        console.timeEnd('timer');
+    });
+});
+
+console.log('::End::');
